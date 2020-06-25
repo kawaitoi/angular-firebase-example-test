fa6c49298952478e78e2c958b04e7d067f3ceed5
kawai toi <kawaitoi123@gmail.com>
2020-06-25 11:11:09 +0900

    feat(create):ペット追加カルーセル

---
+++ src/app/create/create/create.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss']
+})
+export class CreateComponent implements OnInit {
+
+  petIds = [...Array(10)].map((_, i) => i + 1);
+
+  config: SwiperConfigInterface = {
+    loop: true,
+    navigation: true,
+    pagination: {
+      el: '.pager',
+      clickable: true
+    },
+    centeredSlides: true,
+    slidesPerView: 3
+  };
+  selectedPetId = 0;
+
+  form = this.fb.group({
+    name: ['', [
+      Validators.required,
+      Validators.maxLength(10)
+    ]],
+    gender: ['', [
+      Validators.required,
+      Validators.pattern(/male|female/)
+    ]]
+  });
+
+  get nameControl() {
+    return this.form.get('name') as FormControl;
+  }
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+  }
+
+  submit() {
+    console.log(this.form.value);
+    console.log(this.selectedPetId);
+    console.log("お疲れの様でした！");
+  }
+
+}
